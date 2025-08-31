f9cf514bdd93952aaa6ef4bc3c669aa1d549dfe6
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2025-08-30 22:59:48 -0300

    Crear programa principal

---
+++ main.py
@@ -0,0 +1,10 @@
+# importar el modulo calculator
+# from src import calculator
+
+# Previene que el codigo se ejecute al importar el modulo
+if __name__ == "__main__":
+    num = float(input("Enter a number: "))
+    other_num = float(input("Enter another number: "))
+    operation = input("Enter an operation (add, subtract, multiply, divide): ")
+    # result = calculator.calculate(operation, num, other_num)
+    # print(result)

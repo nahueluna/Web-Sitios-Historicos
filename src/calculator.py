80aec120f0d3f9f41e8111a83f158ede1ca2c575
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2025-08-31 13:33:49 -0300

    Crear modulo calculator

---
+++ src/calculator.py
@@ -0,0 +1,22 @@
+# from src import operations
+
+def calculate(operation, a, b):
+    match operation:
+        case "+":
+            # return operations.add(a, b)
+            pass
+
+        case "-":
+            # return operations.subtract(a, b)
+            pass
+
+        case "*":
+            # return operations.multiply(a, b)
+            pass
+
+        case "/":
+            # return operations.divide(a, b)
+            pass
+
+        case _:
+            return "Invalid operation"

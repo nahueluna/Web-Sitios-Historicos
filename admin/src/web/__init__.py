c6b52118d30e6d643b1e5fa1b57e387682dbbff5
Thiago Ramos <thiagoramosnoilhan@gmail.com>
2025-09-08 19:43:26 -0300

    Se agrega carpeta de estaticos y estilos basicos

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask import render_template
 
-def create_app():
-    app = Flask(__name__)
+def create_app(env='development', static_folder='../../static'):
+    app = Flask(__name__, static_folder=static_folder)
 
     @app.route('/')
     def home():

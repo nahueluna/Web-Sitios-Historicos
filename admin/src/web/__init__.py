c2a7ee970e09538137f269c7580b0c5fa3a528e3
Nahuel <nahuelnicolasluna@hotmail.com>
2025-09-16 22:57:08 -0300

    Se registran blueprints para búsqueda avanzada y tags

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
+from src.web.controllers.tags import tags_bp
 from src.web.handlers import error
+from src.web.controllers.advanced_search import advanced_search_bp
 
 def create_app(env='development', static_folder='../../static'):
     app = Flask(__name__, static_folder=static_folder)
@@ -13,4 +15,6 @@ def create_app(env='development', static_folder='../../static'):
     app.register_error_handler(401, error.unauthorized)
     app.register_error_handler(500, error.internal_server_error)
 
+    app.register_blueprint(advanced_search_bp)
+    app.register_blueprint(tags_bp)
     return app

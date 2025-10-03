0c3a72ed6066a0b3dd6d43300ab3e39f53d34e52
Ernesto Cafre <ernesto.cafre@hotmail.com>
2025-10-03 10:44:49 -0300

    html sobre mantenimiento

--- admin/src/web/decorator.py
+++ admin/src/web/decorator.py
@@ -18,20 +18,19 @@ def permission_required(permission_name):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            exists_permission = find_permission_by_name(permission_name)
-            if not exists_permission:
-                print(f"El permiso '{permission_name}' no existe")
-                return redirect(url_for('home'))
+            # Verificar si el usuario está autenticado, puede ser con el decorador acá o en la ruta
+            # En la ruta simplifica las pruebas
             user = find_user_by_id(session['user_id'])
             if not user:
-                print("Usuario no encontrado en permission_required")
-                flash("No tienes permiso para acceder a esta página", "error")
-                return redirect(url_for('home'))
+                flash("Debes iniciar sesión para acceder a esta página", "warning")
+                print("No user found in session")
+                return redirect(url_for('auth.login'))
+            
+            # Utilizar la función has_permission del usuario que maneja toda la lógica
             if not user.has_permission(permission_name):
-                print(f"Usuario {user.id} con rol {user.role} no tiene permiso {permission_name}")
                 flash("No tienes permiso para acceder a esta página", "error")
-                return redirect(url_for('home'))
-            print(f"Usuario {user.id} con rol {user.role} tiene permiso {permission_name}")
+                return redirect(url_for('home.index'))
+            
             return f(*args, **kwargs)
         return decorated_function
     return decorator
@@ -39,23 +38,17 @@ def permission_required(permission_name):
 def system_admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        # Verificar si el usuario está autenticado
+        if 'user_id' not in session:
+            flash("Debes iniciar sesión para acceder a esta página", "warning")
+            print("No hay user_id en session")
+            return redirect(url_for('auth.login'))
+        
         user = find_user_by_id(session['user_id'])
         if not user or not user.system_admin:
             print("Usuario no encontrado o no es sys admin")
             flash("No tienes permiso para acceder a esta página", "error")
-            return redirect(url_for('home'))
+            return redirect(url_for('home.index'))
         print(f"Usuario {user.id} es sys admin")
         return f(*args, **kwargs)
     return decorated_function
-
-
-# def check_maintenance_mode(f):
-#     """
-#     Decorador que verifica si el sistema está en modo mantenimiento.
-#     Si está en modo mantenimiento, redirige a una página de mantenimiento.
-#     """
-#     @login_required
-#     @wraps(f)
-#     def decorated_function(*args, **kwargs):
-#
-#     return decorated_function

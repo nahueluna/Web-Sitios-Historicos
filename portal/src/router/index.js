896680f6e4f4ca06af39e636cda5fa30d607c800
Juani <jibyrab@gmail.com>
2025-10-28 11:39:35 -0300

    Arreglo del merge

--- portal/src/router/index.js
+++ portal/src/router/index.js
@@ -3,10 +3,7 @@ import HomeView from '@/views/HomeView.vue'
 import ProfileView from '@/views/user/ProfileView.vue'
 import { useSessionStore } from '@/stores/sessionStore'
 import { useLoginModalStore } from '@/stores/LoginModalStore'
-import HomeView from '@/views/HomeView.vue'
-import ProfileView from '@/views/user/ProfileView.vue'
-import { useSessionStore } from '@/stores/sessionStore'
-import { useLoginModalStore } from '@/stores/LoginModalStore'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,21 +18,25 @@ const router = createRouter({
       path: '/sites',
       name: 'sites',
       component: () => import('../views/SitesList.vue'),
+      meta: { requiresAuth: false }
     },
     {
       path: '/sites/:slug',
       name: 'site-detail',
       component: () => import('../views/SiteDetail.vue'),
+      meta: { requiresAuth: false }
     },
     {
       path: '/sites',
       name: 'sites',
       component: () => import('../views/SitesList.vue'),
+      meta: { requiresAuth: false }
     },
     {
       path: '/sites/:slug',
       name: 'site-detail',
       component: () => import('../views/SiteDetail.vue'),
+      meta: { requiresAuth: false }
     },
     {
       path: '/about',
@@ -45,7 +46,50 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../views/user/ProfileView.vue'),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/reviews',
+      name: 'reviews',
+      component: () => import('../views/user/ReviewsView.vue'),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/favorites',
+      name: 'favorites',
+      component: () => import('../views/user/FavoritosView.vue'),
+      meta: { requiresAuth: true }
+    }
   ],
 })
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const guestOnly = to.matched.some(record => record.meta.guestOnly);
+  const sessionStore = useSessionStore();
+  const loginModalStore = useLoginModalStore();
+
+  const data = localStorage.getItem('user');
+  if (!sessionStore.isAuthenticated() && data) {
+    sessionStore.user = JSON.parse(data);
+  }
+
+  if (requiresAuth && !sessionStore.isAuthenticated()) {
+    //alert("Debe iniciar sesión para acceder a este recurso.");
+    sessionStore.redirect_uri = to.fullPath;
+    loginModalStore.openLoginModal();
+    next(false);
+  } else if (guestOnly && sessionStore.isAuthenticated()) {
+    alert("Ya ha iniciado sesión.");
+    next(from.fullPath);
+  } else {
+    next();
+  }
+});
+
 
 export default router

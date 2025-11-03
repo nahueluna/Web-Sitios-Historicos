8aa14f6d9ad80a701d9e69ca170e010a67268894
Ernesto Cafre <ernesto.cafre@hotmail.com>
2025-11-03 09:13:26 -0300

    login

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -11,10 +11,12 @@ from src.web.controllers.historic_sites import historic_sites_bp
 from src.web.controllers.historic_sites import render_index
 from src.web.controllers.role_permission import role_bp
 from src.web.controllers.user_controller import bp_user
+from src.web.controllers.google_login import bp_google_auth
 import src.web.controllers.advanced_search
 from src.web.controllers.auth import bp_auth
 from src.web.controllers.feature_flag import feature_flag_bp
 from src.core.bcrypt import bcrypt
+from flask_cors import CORS
 
 
 session = Session()
@@ -29,6 +31,12 @@ def create_app(env='development', static_folder='../../static'):
 
     bcrypt.init_app(app)
 
+    ## Necesario para el OAuth2 con Google
+    CORS(app, origins=["http://localhost:5173"], supports_credentials=True)  # URL de tu frontend Vue
+    app.secret_key = "dev_secret_key"  # Deberia ser un valor seguro en producción
+    app.config["SESSION_COOKIE_SAMESITE"] = "None"
+    app.config["SESSION_COOKIE_SECURE"] = True
+
     @app.route('/')
     def home():  # return render_index()
         if is_authenticated():
@@ -36,21 +44,21 @@ def create_app(env='development', static_folder='../../static'):
         else:
             return redirect(url_for("auth.login"))
 
-    @app.route('/admin')
-    def admin():
-        return render_template('layout.html')
+    #@app.route('/admin')
+    #def admin():
+    #    return render_template('layout.html')
 
-    @app.route('/admin/validacion-propuestas')
-    def validacion_propuestas():
-        return render_template('validacion_propuestas.html')
+    #@app.route('/admin/validacion-propuestas')
+    #def validacion_propuestas():
+    #    return render_template('validacion_propuestas.html')
 
-    @app.route('/admin/moderacion')
-    def moderacion():
-        return render_template('moderacion.html')
+    #@app.route('/admin/moderacion')
+    #def moderacion():
+    #    return render_template('moderacion.html')
 
-    @app.route('/admin/gestion-usuarios')
-    def gestion_usuarios():
-        return render_template('gestion_usuarios.html')
+    #@app.route('/admin/gestion-usuarios')
+    #def gestion_usuarios():
+    #    return render_template('gestion_usuarios.html')
 
     app.register_error_handler(404, error.not_found)
     app.register_error_handler(401, error.unauthorized)
@@ -72,6 +80,8 @@ def create_app(env='development', static_folder='../../static'):
     app.register_blueprint(role_bp)
     app.register_blueprint(bp_auth)
     app.register_blueprint(feature_flag_bp)
+    app.register_blueprint(bp_google_auth)
+
 
     @app.cli.command("reset-db")
     def reset_db():

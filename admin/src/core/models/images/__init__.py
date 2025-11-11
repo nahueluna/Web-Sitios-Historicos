0d69b3504b1b4ff88edb4d45807a9a85c947ebe6
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2025-11-11 13:46:41 -0300

    Permitir editar las imagenes de un sitio
    Ahora es posible agregar, eliminar y ordenar las imagenes.

--- admin/src/core/models/images/__init__.py
+++ admin/src/core/models/images/__init__.py
@@ -1,10 +1,22 @@
 from src.core.database import db
 from src.core.models.images.image import Image
 from sqlalchemy.exc import IntegrityError
+from flask import current_app
 
 class ErrorAlGuardar(Exception):
     """Ocurrio un error al guardar las imagenes."""
     pass
+
+def get_bucket_url():
+    server = current_app.config["MINIO_SERVER"]
+    secure = current_app.config["MINIO_SECURE"]
+    bucket = current_app.config["MINIO_BUCKET"]
+    if secure:
+        secure = "s"
+    else:
+        secure = ""
+    return f"http{secure}://{server}/{bucket}"
+
 
 def guardar_imagenes(nombres, titulos, descripciones, sitio):
     for i in range(len(nombres)):
@@ -22,3 +34,10 @@ def guardar_imagenes(nombres, titulos, descripciones, sitio):
     except IntegrityError:
         db.session.rollback()
         raise ErrorAlGuardar
+
+def get_images_by_site(id):
+    bucket_url = get_bucket_url()
+    images = db.session.query(Image).filter_by(sitio=id).order_by(Image.orden).all()
+    for image in images:
+        image.url = f"{bucket_url}/{image.nombre}"
+    return images

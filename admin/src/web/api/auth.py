1305841570c362ba0ccca1d72aa47a7794948d68
Thiago Ramos <thiagoramosnoilhan@gmail.com>
2025-11-14 16:55:25 -0300

    Implementar access y refresh token con endpoints correspondientes

--- admin/src/web/api/auth.py
+++ admin/src/web/api/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, session, current_app
 from sqlalchemy.exc import SQLAlchemyError
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 from datetime import timedelta
 from src.web.handlers.auth import is_authenticated
 from src.core.models.auth import get_usuario_by_email
@@ -17,26 +17,49 @@ def generate_jwt_token():
     
     user = get_usuario_by_email(session["user"])
     if not user:
-        # Para en caso de sesiones truchas? ns
         return jsonify({"error": {
-            "code": "invalid_credentials",
-            "message": "Credenciales inválidas."
+            "code": "user_not_found",
+            "message": "Usuario no existe"
         }}), 401
     
     if not user.activo:
         return jsonify({"error": {
-            "code": "access_denied",
-            "message": "Credenciales no corresponden a un usuario activo."
+            "code": "user_blocked",
+            "message": "Usuario bloqueado"
         }}), 403
     
-    # Crear token 
+    # Crear access y refresh tokens
     access_token = create_access_token(identity=user.id)
+    refresh_token = create_refresh_token(identity=user.id)
+
+    return jsonify({
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+    }), 200
+
+@auth_api.post('/refresh')
+@jwt_required(refresh=True)  # Deberia verificar automaticamente el refresh token
+def refresh():
+    current_user_id = get_jwt_identity()
+
+    user = get_usuario_by_email(session["user"])
     
-    # Obtener tiempo en segundos de expiracion desde la configuracion
-    expires_delta = current_app.config.get('JWT_ACCESS_TOKEN_EXPIRES')
-    total_seconds = int(expires_delta.total_seconds())
+    # Usuario eliminano
+    if not user:
+        return jsonify({"error": {
+            "code": "user_not_found", 
+            "message": "Usuario no existe"
+        }}), 401
+    
+    # Usuario bloqueado
+    if not user.activo:
+        return jsonify({"error": {
+            "code": "user_blocked",      
+            "message": "Usuario bloqueado"
+        }}), 403  
+    
+    new_access_token = create_access_token(identity=current_user_id)
     
     return jsonify({
-        "token": access_token,
-        "expires_in": total_seconds
+        "access_token": new_access_token,
     }), 200

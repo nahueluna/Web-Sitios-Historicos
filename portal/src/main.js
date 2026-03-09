c3144e6a29862b57dbadf5582dde19f11d4ef07f
c97ernesto <ernesto-cafre@outlook.com>
2026-03-09 12:56:39 -0300

    fix(main): delete import wildcard de bootstrap-vue-next no utilizado.

--- portal/src/main.js
+++ portal/src/main.js
@@ -9,18 +9,15 @@ import vue3GoogleLogin from 'vue3-google-login'
 
 import App from './App.vue'
 import router from './router'
-import * as BootstrapVueNext from 'bootstrap-vue-next'
 
 // Importar CSS y JS de Bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(BootstrapVueNext.BootstrapVueNext)
 
 app.use(vue3GoogleLogin, {
   clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID
